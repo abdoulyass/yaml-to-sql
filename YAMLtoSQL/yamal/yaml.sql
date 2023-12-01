43aac32f51feab16a150eed8b564222962512285
unknown <perruce4356@gmail.com>
2023-12-02 00:16:12 +0100

    Your commit message here

--- YAMLtoSQL/yamal/yaml.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO table1 (column1, column2) VALUES ('value1', 'value2');
-INSERT INTO table2 (column1, column2) VALUES ('value3', 'value4');
-INSERT INTO yassir (ABD) VALUES ('10');
-INSERT INTO table4 (column1, column2) VALUES ('value1', 'value2');
-INSERT INTO table8 (column1, column2) VALUES ('value1', 'value2');
